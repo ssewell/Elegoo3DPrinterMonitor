c92322d7a7ec5523217682a4462a3d88eaf7905b
Shane Sewell <2242776+ssewell@users.noreply.github.com>
2023-09-04 22:38:03 -0500

    Menu adjustments

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -161,20 +161,6 @@ export default class MenuBuilder {
           },
         },
         {
-          label: 'Documentation',
-          click() {
-            shell.openExternal(
-              'https://github.com/electron/electron/tree/main/docs#readme'
-            );
-          },
-        },
-        {
-          label: 'Community Discussions',
-          click() {
-            shell.openExternal('https://www.electronjs.org/community');
-          },
-        },
-        {
           label: 'Search Issues',
           click() {
             shell.openExternal('https://github.com/electron/electron/issues');
@@ -259,20 +245,6 @@ export default class MenuBuilder {
             label: 'Learn More',
             click() {
               shell.openExternal('https://electronjs.org');
-            },
-          },
-          {
-            label: 'Documentation',
-            click() {
-              shell.openExternal(
-                'https://github.com/electron/electron/tree/main/docs#readme'
-              );
-            },
-          },
-          {
-            label: 'Community Discussions',
-            click() {
-              shell.openExternal('https://www.electronjs.org/community');
             },
           },
           {
