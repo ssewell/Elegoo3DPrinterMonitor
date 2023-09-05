9b48a28237ae40a51c09d65fcddebc5e3203479d
Shane Sewell <2242776+ssewell@users.noreply.github.com>
2023-09-04 21:54:26 -0500

    Update default size of main window

--- src/main/main.ts
+++ src/main/main.ts
@@ -72,8 +72,8 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 700,
+    height: 440,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: app.isPackaged
