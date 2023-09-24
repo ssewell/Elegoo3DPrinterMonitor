9be5d64f0b5e467c9a453419533b6dbfcc7b0ec8
Shane Sewell <2242776+ssewell@users.noreply.github.com>
2023-09-24 00:41:01 -0500

    Fix hr that shouldn't be present when there's no time estimate

--- src/components/TimeStatus.tsx
+++ src/components/TimeStatus.tsx
@@ -10,16 +10,19 @@ export default function TimeStatus({
   const { hours, minutes } = ticksToTime(ticks);
 
   return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
-      <hr className="subtle" />
       {ticks > 0 && (
-        <div className="subtle">
-          {hours > 0 ? `${hours} ${hours === 1 ? 'hour' : 'hours'}, ` : ''}
-          {minutes > 0
-            ? `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}`
-            : 'Less than one minute'}{' '}
-          {label}
-        </div>
+        <>
+          <hr className="subtle" />
+          <div className="subtle">
+            {hours > 0 ? `${hours} ${hours === 1 ? 'hour' : 'hours'}, ` : ''}
+            {minutes > 0
+              ? `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}`
+              : 'Less than one minute'}{' '}
+            {label}
+          </div>
+        </>
       )}
     </>
   );
